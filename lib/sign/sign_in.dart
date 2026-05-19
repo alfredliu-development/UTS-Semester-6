bbfaa3a2c045da2b6999394d4bc0fa6d6a5ae19b
Alfred Liusten <alfredliu.development@gmail.com>
2026-05-19 19:26:21 +0700

    Part 2

--- lib/sign/sign_in.dart
+++ lib/sign/sign_in.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -28,113 +31,195 @@ class SignInState extends State<SignIn> {
         ),
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(height: 20),
-          Center(
-            child: Image.asset(
-              "assets/image/my_logo.png",
-              height: 120,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20),
+            Center(
+              child: Image.asset(
+                "assets/image/my_logo.png",
+                height: 120,
+              ),
             ),
-          ),
-
-          SizedBox(height: 40),
-          Container(
-            margin: EdgeInsets.symmetric(
-              vertical: 20
-            ),
-
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: "Hint yours e-mail",
-                    prefixIcon: Icon(Icons.email_outlined),
-                    prefixIconColor: WidgetStateColor.resolveWith((states) {
-                      return states.contains(WidgetState.focused) ? Colors.lightBlueAccent : Colors.black54;
-                    }),
-
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-
-                    labelStyle: TextStyle(
-                      color: Colors.black54,
-                      fontWeight: FontWeight.bold
-                    ),
-
-                    floatingLabelStyle: TextStyle(
-                      color: Colors.lightBlueAccent,
-                      fontWeight: FontWeight.w500
-                    ),
-                  ),
-
-                  validator: (value) {
-                    return value == null || value.isEmpty ? "Email isn't null" : null;
-                  },
-                ),
-
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: _isPasswordVisible,
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "Hint your password",
-                      prefixIcon: Icon(Icons.lock_outline),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                          color: Colors.black54,
-                        ),
-
-                        onPressed: () {
-                          setState(() => _isPasswordVisible = !_isPasswordVisible);
-                        },
-                      ),
-
+        
+            SizedBox(height: 40),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 20
+              ),
+        
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      hintText: "Hint yours e-mail",
+                      prefixIcon: Icon(Icons.email_outlined),
                       prefixIconColor: WidgetStateColor.resolveWith((states) {
                         return states.contains(WidgetState.focused) ? Colors.lightBlueAccent : Colors.black54;
                       }),
-
+        
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10)
                       ),
-
+        
                       labelStyle: TextStyle(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.bold
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold
                       ),
-
-                      floatingLabelStyle: TextStyle(color: Colors.lightBlueAccent)
+        
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+        
+                    validator: (value) {
+                      return value == null || value.isEmpty ? "Email isn't null" : null;
+                    },
                   ),
-
-                  validator: (value) {
-                    return value == null || value.isEmpty ? "Password isn't null" : null;
-                  },
+        
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: _isPasswordVisible,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Hint your password",
+                        prefixIcon: Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.black54,
+                          ),
+        
+                          onPressed: () {
+                            setState(() => _isPasswordVisible = !_isPasswordVisible);
+                          },
+                        ),
+        
+                        prefixIconColor: WidgetStateColor.resolveWith((states) {
+                          return states.contains(WidgetState.focused) ? Colors.lightBlueAccent : Colors.black54;
+                        }),
+        
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+        
+                        labelStyle: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold
+                        ),
+        
+                        floatingLabelStyle: TextStyle(color: Colors.lightBlueAccent)
+                    ),
+        
+                    validator: (value) {
+                      return value == null || value.isEmpty ? "Password isn't null" : null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+        
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(18),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                  ),
+        
+                  backgroundColor: Colors.lightBlueAccent,
+                  foregroundColor: Colors.white
                 ),
+        
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),
+                ),
+        
+                onPressed: () {
+                  if (_fromKey.currentState!.validate()) {
+                    Navigator.pushReplacementNamed(context, "/home");
+                    return;
+                  }
+                },
+              ),
+            ),
+        
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Didn't have account?"),
+                SizedBox(width: 20),
+                TextButton(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+        
+                  onPressed: () => loginUser(),
+                )
               ],
             ),
-          ),
-
-          SizedBox(height: 20),
-          Center(
-            child: ElevatedButton(
-              child: Text(
-                "Sign In",
-              ),
-
-              onPressed: () {
-              },
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> loginUser() async {
+    final String url = "http://127.0.0.1:8000/api/sign-in/";
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: { "Content-Type": "application/json" },
+        body: jsonEncode({
+          "email": _emailController.text,
+          "password": _passwordController.text
+        }),
+      );
+
+      final data = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(data["message"]),
+          )
+        );
+
+        Navigator.pushReplacementNamed(context, "/home");
+      }
+
+      else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Error: ${data["message"]}"),
+          )
+        );
+      }
+    }
+
+    catch (e) {
+      print("Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Error server is disconnect"),
+        )
+      );
+    }
   }
 }
